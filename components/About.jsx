561499251854cbc90851f0a3bdfead84eeeccb19
z3nn13 <paulphyo25@gmail.com>
2024-05-08 21:32:07 +0700

    fix(*): improve responsiveness

--- components/About.jsx
+++ components/About.jsx
@@ -7,14 +7,14 @@ export default function About() {
             </div>
 
             <div className="flex flex-col w-full justify-center items-center text-center px-16 md:flex-row md:px-48 gap-10">
-
                 <div className="mt-16 max-w-3xl">
                     <p className="font-bold text-md mb-12">
-                        Graduated with Bachelor of Science in Computing <br />- 1st Class Honours <br /> - University Of Greenwich
+                        Graduated with Bachelor of Science in Computing  <br /> - University Of Greenwich  <br />(1st Class Honours)
                     </p>
 
-                    Passionate developer dedicated to learning newfound technologies, solving complex problems and transforming dreams into reality.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores inventore error voluptates nisi voluptate totam temporibus earum maiores culpa impedit dolorem id doloribus nemo minima commodi consequatur, labore dolorum at aliquam assumenda sed laboriosam eaque dolore. Soluta repellendus impedit debitis similique, optio dignissimos porro alias nulla sunt corrupti temporibus labore!
+                    Passionate developer enthusiasted for new knowledge.<br />
+                    Avid enjoyer of guard clausing and abstraction.<br />
+
                 </div>
                 <Image src={"./assets/about.jpg"} width={300} height={300} />
             </div>
--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,14 +1,14 @@
 export default function ContactForm() {
     return (
-        <form className="mt-16 contact-form mx-auto max-w-min mb-16 flex flex-col gap-4">
-            <div className="form-field flex gap-6">
-                <div className="form-item">
+        <form className="contact-form flex flex-col gap-4 w-full px-16  py-16 md:px-60">
+            <div className="form-field flex gap-6 flex-col w-full md:flex-row">
+                <div className="form-item w-full">
                     <label htmlFor="first_name" className="font-semibold text-sm">First Name</label>
-                    <input type="text" className="border-gray-300 border-2 rounded py-1" name="first_name" id="firstName" />
+                    <input type="text" className="border-gray-300 border-2 rounded py-1 w-full" name="first_name" id="firstName" />
                 </div>
-                <div className="form-item">
+                <div className="form-item w-full">
                     <label htmlFor="first_name" className="font-semibold text-sm">Last Name</label>
-                    <input type="text" className="border-gray-300 border-2 rounded py-1" name="last_name" id="lastName" />
+                    <input type="text" className="border-gray-300 border-2 rounded py-1 w-full" name="last_name" id="lastName" />
                 </div>
             </div>
             <div className="form-item flex flex-col">
